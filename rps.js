52260dcd0c5d6fdc9fe1f2345b82c1f637bbdb5f
amit <48701428+amitnit91@users.noreply.github.com>
2024-02-26 18:15:06 +0530

    Added UI to rps game and changed function
    Added a style.css and changed function as per UIin js file

--- rps.js
+++ rps.js
@@ -37,28 +37,54 @@ function playRound(playerSelection,computerSelection){
 function playGame(){
     const btn=document.querySelectorAll('.btn');
     const displayResult=document.querySelector('#display-result');
+    const player=document.querySelector('#player');
+    const computer=document.querySelector('#computer');
     for(val of btn){
         val.addEventListener('click',function(event){
+           if(win<5 && lose<5){
             let playerSelection=event.target.name;
             let computerSelection=getComputerChoice();
             let roundResult=playRound(playerSelection,computerSelection);
             console.log(roundResult,win,lose);
+            player.textContent=`Player :${win}`
+            computer.textContent=`Computer :${lose}`;
             displayResult.textContent=roundResult;
+           }
+            if(win===5){
+                displayResult.textContent=`You WIN the game,with score :- win:${win}, lose:${lose}, tie:${tie} ,Play Again `;
+                return;
+            }
+            else if(lose===5){
+                displayResult.textContent=`You LOSE the game,with score :- win:${win}, lose:${lose}, tie:${tie} ,Play Again `;
+                return;
+            }
+            else if(win===5 && lose===5){
+                displayResult.textContent=`Game is a TIE, with score :- win:${win}, lose:${lose}, tie:${tie} ,Play Again `;
+            return;
+            }    
+                     
         });
-    }       
-    
-    if(win===5){
-        displayResult.textContent=`You WIN the game,with score -win:${win}, lose:${lose}, tie:${tie}`;
-        return;
-    }
-    else if(lose===5){
-        displayResult.textContent=`You LOSE the game,with score -win:${win}, lose:${lose}, tie:${tie}`;
-        return;
-    }
-    else if(win===5 && lose===5){
-        displayResult.textContent=`Game is a TIE, with score -win:${win}, lose:${lose}, tie:${tie}`;
-        return;
-    }
+         
+   
 }
+        
+    }   
+    
+    function playAgain(){
+        const playBtn=document.querySelector('#play-again');
+        // playBtn.addEventListener('click',function(e){
+            const player=document.querySelector('#player');
+            const computer=document.querySelector('#computer');
+            const displayResult=document.querySelector('#display-result');
+            win=0;lose=0;tie=0;
+            player.textContent=`Player :${win}`
+            computer.textContent=`Computer :${lose}`;
+            displayResult.textContent="click  your choice";
+            e.stopPropagation();
+            
+        // });
+
+    }
+   
 
 playGame();
